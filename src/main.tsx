38dd527d4dd852f196b0f8160438c2b2d2f26837
Louie Morais <louie.laicos@gmail.com>
2025-10-17 20:44:33 +0100

    test(qa): testing db connections and ci

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
+// src/main.tsx
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { testDatabaseConnection } from './lib/testConnection';
+
+// Test database connectivity on app start
+void testDatabaseConnection();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
