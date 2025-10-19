e9b417f6feead2efccedd8897e10bd4d4d253f7c
Louie Morais <louie.laicos@gmail.com>
2025-10-19 16:51:27 +0100

    chore(config): updated configs for sprint

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,24 @@
+/** @type {import('jest').Config} */
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json' }],
+    // ↓ NEW: let Jest handle pure-ESM modules through Babel
+    '^.+\\.[cm]?[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    // ↓ NEW: compile uuid and other ESM packages
+    'node_modules/(?!(uuid)/)',
+  ],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
@@ -15,11 +26,6 @@ export default {
     '!src/vite-env.d.ts',
   ],
   coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
+    global: { branches: 70, functions: 70, lines: 70, statements: 70 },
   },
 };
