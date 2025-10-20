4907f2f904ea4095c530c296faad3e8220751866
Louie Morais <louie.laicos@gmail.com>
2025-10-20 15:44:27 +0100

    Tooling(Jest): Freeze minimal working Jest implementation - deleted duplicated setupTests.ts

--- setupTests.ts
+++
@@ -1,8 +0,0 @@
-// src/setupTests.ts
-import '@testing-library/jest-dom';
-
-// Optional: if your code uses fetch, provide a minimal mock
-if (!globalThis.fetch) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  globalThis.fetch = (jest.fn() as unknown) as typeof fetch;
-}
